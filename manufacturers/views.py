dd3eb3298f09f435ba9cf3e20030f8c37b49d251
Vitor Carvalho <vtocarvalho2000@gmail.com>
2025-12-24 15:25:35 -0300

    feat: Enhance Manufacturers API documentation with detailed schema descriptions

--- manufacturers/views.py
+++ manufacturers/views.py
@@ -1,17 +1,48 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from app.permissions import GlobalDefaultPermissions
 from manufacturers.models import Manufacturers
 from manufacturers.serializers import ManufacturersSerializer
 
 
+@extend_schema_view(
+    get=extend_schema(
+        description='Retorna todos os fabricantes e seus dados',
+        responses={200: ManufacturersSerializer}
+    ),
+    post=extend_schema(
+        description='Criar um fabricante',
+        request=ManufacturersSerializer,
+        responses={200: ManufacturersSerializer}
+    )
+)
 class ManufacturersListCreateAPIView(generics.ListCreateAPIView):
 
     permission_classes = (IsAuthenticated, GlobalDefaultPermissions,)
     queryset = Manufacturers.objects.all()
     serializer_class = ManufacturersSerializer
 
-
+@extend_schema_view(
+    get=extend_schema(
+        description='Retorna os dados de um fabricante específico',
+        responses={200: ManufacturersSerializer}
+    ),
+    put=extend_schema(
+        description='Atualiza todos os dados de um fabricante',
+        request=ManufacturersSerializer,
+        responses={200: ManufacturersSerializer}
+    ),
+    patch=extend_schema(
+        description='Atualiza parcialmente os dados de um fabricante',
+        request=ManufacturersSerializer,
+        responses={200, ManufacturersSerializer}
+    ),
+    delete=extend_schema(
+        description='Remove um fabricante do sistema',
+        responses={204: None}
+    )
+)
 class ManufacturersRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
 
     permission_classes = (IsAuthenticated, GlobalDefaultPermissions,)
