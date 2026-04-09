730e625fc7abed66e7a5eb3a9794ba33e960559f
Vitor Carvalho <vtocarvalho2000@gmail.com>
2026-04-09 20:45:08 -0300

    refactor(deploy): create superuser only if not exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
-echo "Apply migrations..."
+echo "Applying migrations..."
 python manage.py migrate --noinput
 
 echo "Collect static..."
 python manage.py collectstatic --noinput
 
-echo "Create superuser automatically..."
-python manage.py createsuperuser --noinput || true
+echo "Checking superuser..."
 
-echo "Starting server..."
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+
+if username and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=os.getenv("DJANGO_SUPERUSER_EMAIL"),
+        password=os.getenv("DJANGO_SUPERUSER_PASSWORD")
+    )
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
+END
+
+echo "Starting Gunicorn..."
 gunicorn app.wsgi:application --bind 0.0.0.0:$PORT
